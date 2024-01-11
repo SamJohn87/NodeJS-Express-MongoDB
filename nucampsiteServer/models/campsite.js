d003fa7315da4810d3a597eab2ffbbd239ef7b12
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2024-01-10 20:51:00 -0600

    Mongoose Population

--- nucampsiteServer/models/campsite.js
+++ nucampsiteServer/models/campsite.js
@@ -17,8 +17,8 @@ const commentSchema = new Schema({
         required: true
     },
     author: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId, //store reference to a User document id
+        ref: 'User'
     }
 }, {
     timestamps: true
