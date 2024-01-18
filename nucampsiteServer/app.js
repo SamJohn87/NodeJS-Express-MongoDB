ffab596a385da095dbb107464904ee26040bb5a1
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2024-01-17 18:39:41 -0600

    File upload using multer

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -10,6 +10,7 @@ const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 const url = config.mongoUrl;
@@ -25,7 +26,7 @@ connect.then(() => console.log('Connected correctly to server'), err => console.
 const app = express();
 
 app.all('*', (req, res, next) => { //redirect to secure connection
-  if(req.secure) { //https connection
+  if (req.secure) { //https connection
     return next();
   } else {
     console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
@@ -51,6 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
